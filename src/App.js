9a293e7133f7742ae2fd2c1747a5da0e20934b9e
Custer Murwamuila <custer.murwamuila@weconnect.tech>
2024-06-13 15:42:16 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import "./styles.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import MoviesGrid from "./components/MoviesGrid";
+import MyWatchList from "./components/MyWatchList"; // Updated import
+
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [watchList, setWatchList] = useState([]); // Define watchList state variable
+
+  useEffect(() => {
+    fetch("movies.json")
+      .then((response) => response.json())
+      .then((data) => setMovies(data));
+  }, []);
+
+  const toggleWatchList = (movieId) => {
+    setWatchList((prev) =>
+      prev.includes(movieId)
+        ? prev.filter((id) => id !== movieId)
+        : [...prev, movieId]
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <Header />
+
+        <Router>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/watchlist">WatchList</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <MoviesGrid
+                  watchList={watchList} // Pass watchList to MoviesGrid
+                  movies={movies}
+                  toggleWatchList={toggleWatchList}
+                />
+              }
+            />
+            <Route
+              path="/watchlist"
+              element={
+                <MyWatchList
+                  watchList={watchList} // Pass watchList to MyWatchList
+                  movies={movies}
+                  toggleWatchList={toggleWatchList}
+                />
+              }
+            />
+          </Routes>
+        </Router>
+      </div>
+
+      <Footer />
     </div>
   );
 }
